4ad3f0989f5ca2c8937fab5e7df8920dc3da228a
Dann Cuapiaco <cuapiaco.dann@yahoo.com>
2022-08-30 12:01:14 +0800

    add st.secrets

--- apps/auth.py
+++ apps/auth.py
@@ -2,9 +2,15 @@
 import os
 from dotenv import load_dotenv
 from binance.client import Client
+import streamlit as st
 
 # %% LOAD CLIENT
 load_dotenv()
 api_key = os.environ.get('api_key')
 api_secret = os.environ.get('api_secret')
 client = Client(api_key, api_secret)
+
+# %% STREAMLIT LOAD CLIENT
+api_key = st.secrets['api_key']
+api_secret = st.secrets['api_secret']
+st_client = Client(api_key, api_secret)
