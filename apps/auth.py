5f27f1a87c8e0c418ac5c58ac4651635c26d4195
Dann Cuapiaco <cuapiaco.dann@yahoo.com>
2022-09-27 17:38:30 +0800

    edit auth

--- apps/auth.py
+++ apps/auth.py
@@ -9,8 +9,3 @@ load_dotenv()
 api_key = os.environ.get('api_key')
 api_secret = os.environ.get('api_secret')
 client = Client(api_key, api_secret)
-
-# %% LOAD CLIENT
-api_key = os.environ.get('api_key')
-api_secret = os.environ.get('api_secret')
-st_client = Client(api_key, api_secret)
--- apps/get_symbols.py
+++ apps/get_symbols.py
@@ -1,8 +1,8 @@
 # %%
-from .auth import client, st_client
+from .auth import client
 from .clean_pairs import clean_trading_pairs
 
 # %% GET SYMBOL FROM BINANCE
-futures_exchange_info = st_client.futures_exchange_info()
+futures_exchange_info = client.futures_exchange_info()
 trading_pairs = clean_trading_pairs(futures_exchange_info)
 symbols = [info['Symbol']+'PERP' for info in trading_pairs]
